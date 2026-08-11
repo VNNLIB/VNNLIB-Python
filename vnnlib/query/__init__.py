2acf43a728c281980e864189c4ec8c489db48ce4
Shohab_baller <23479079@student.uwa.edu.au>
2026-08-11 14:24:40 +0800

    Added vnnlib.query subpackage skeleton with boolean expression types

---
+++ vnnlib/query/__init__.py
@@ -0,0 +1,29 @@
+"""Boolean query expression types for the VNNLib Python bindings."""
+
+from .._core import (
+    BoolExpr,
+    Comparison,
+    GreaterThan,
+    GreaterEqual,
+    LessThan,
+    LessEqual,
+    Equal,
+    NotEqual,
+    Connective,
+    And,
+    Or,
+)
+
+__all__ = [
+    "BoolExpr",
+    "Comparison",
+    "GreaterThan",
+    "GreaterEqual",
+    "LessThan",
+    "LessEqual",
+    "Equal",
+    "NotEqual",
+    "Connective",
+    "And",
+    "Or",
+]
